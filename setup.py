a88d5c424d0ccc1ac159365a51f2d5f8e44163a6
ulaştaylan <94192632+ulasdesouza24@users.noreply.github.com>
2026-03-02 01:08:54 +0300

    Bump version from 0.1.0 to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='consensus-fs',
-    version='0.1.0',
+    version='0.1.1',
     description='Ensemble Feature Selection Library',
     author='Ulaş Taylan Met',
     author_email='umet9711@gmail.com',
